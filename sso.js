ba3cb1e053bc57b53a6598063f5ad4c6f88ecf2f
Tom <tomtharakanlt@gmail.com>
2022-07-26 21:45:30 +0530

    Create sso.js

---
+++ sso.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+if(window.location.includes("GenericSso")) {
+   location.href = "https://testarea.pfpleisure.org/LhWebTest/en/Members/MyBookings";
+}
+}, false);
