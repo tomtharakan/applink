21570bb4f9fb3e7e5f33db09a579cd5635c3f292
Tom <tomtharakanlt@gmail.com>
2022-07-26 21:53:36 +0530

    Update sso.js

--- sso.js
+++ sso.js
@@ -1,6 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-if(window.location.includes("GenericSso")) {
-   location.href = "https://testarea.pfpleisure.org/LhWebTest/en/Members/MyBookings";
-}
+location.href = "https://testarea.pfpleisure.org/LhWebTest/en/Members/MyBookings";
 }, false);
