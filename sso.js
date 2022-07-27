5f95e19141dc71f72d43a5e5586f618f0b9a0514
Tom <tomtharakanlt@gmail.com>
2022-07-27 10:18:43 +0530

    Update sso.js

--- sso.js
+++ sso.js
@@ -1,4 +1,4 @@
 if(window.location.href.includes("Home")) {
-  window.location.assign("https://testarea.pfpleisure.org/LhWebTest/en/Members/MyBookings")
+  window.location.assign("https://testarea.pfpleisure.org/LhWebTest/en/Members/MyPayments")
    window.webkit.messageHandlers.handleNativeAction.postMessage({"type":"stopLoading", "key":"val"});
   }
